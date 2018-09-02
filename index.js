8a77e5d9d1e8843dbb39a0430540e3c2832c69eb
github username <github email address>
2018-09-01 20:04:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+let issues = document.getElementById('issues')
+let lab = 'bryanteng/javascript-fetch-lab'
+
 function getIssues() {
+  fetch(`https://api.github.com/repos/${lab}/issues`, {
+  method: `get`,
+  headers: {
+    Authorization: `token ${getToken()}`
+  }
+}).then(res => console.log(res));
 }
 
 function showIssues(json) {
+
 }
 
 function createIssue() {
+
+  fetch(`https://api.github.com/repos/${lab}/issues`, {
+  method: `post`,
+  title: document.getElementById('title'),
+  body: document.getElementById('body'),
+  headers: {
+    Authorization: `token ${getToken()}`
+  }
+}).then(res => console.log(res))
 }
 
 function showResults(json) {
@@ -13,6 +32,12 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+  method: `post`,
+  headers: {
+    Authorization: `token ${getToken()}`
+  }
+}).then(res => console.log(res));
 }
 
 function getToken() {
